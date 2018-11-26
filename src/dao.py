acc753a1d5a733585cd0e569d8ed26bd5b7fd83c
ryun cha <ryunx21@gmail.com>
2018-11-26 15:14:25 +0900

    raw 데이터 수정 ("Date" 값이 테이블 모양을 깨트려서)

--- src/dao.py
+++ src/dao.py
@@ -1,4 +1,10 @@
 import pandas as pd
 import numpy as np
 
+load = pd.read_csv("../raw/CurrentUsage.csv")
+pv = pd.read_csv("../raw/EnergyStoragePerformance.csv")
+es = pd.read_csv("../raw/SolarPVGeneration.csv")
+net = pd.read_csv("../raw/NetUsage.csv")
+
+
 
